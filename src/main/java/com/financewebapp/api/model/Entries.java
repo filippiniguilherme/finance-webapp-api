73ca28650663b648bb17b786cb17f98322b50099
vfinger <vinifinger92@gmail.com>
2022-07-31 11:45:43 -0300

    Change structure of project and add HealthcheckController

---
+++ src/main/java/com/financewebapp/api/model/Entries.java
@@ -0,0 +1,43 @@
+package com.financewebapp.api.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity(name = "entries")
+@Data
+public class Entries {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "value")
+    private Double value;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+}
